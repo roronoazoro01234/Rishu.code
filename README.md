69e568e9205ee8afa5b7dd56179ff3eb4619f333
roronoazoro01234 <godzopgamer@gmail.com>
2024-07-17 21:25:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rishu.code.cpp
+ 
---
+++ sum.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+int main(){
+    int a,b,sum;
+    cout<<"enter the first no:"<<endl;
+    cin>>a;
+    cout<<"enter the second no:"<<endl;
+    cin>>b;
+    
+    sum= a+b;
+    cout<<"The sum is:"<<sum<<endl;
+
+    return 0;
+
+} 
