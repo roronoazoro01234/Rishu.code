4c685c0c871a211dcb81ae63243b1601fb659467
roronoazoro01234 <godzopgamer@gmail.com>
2024-07-27 21:04:04 +0530

    Add files via upload

---
+++ nCrFormula.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+using namespace std;
+
+int fact(int n){
+int factorial=1;
+  for(int i=2;i<=n;i++){
+   factorial=factorial*i;
+  }
+  return factorial;
+}
+  int main(){
+    int n;
+    cout<<"enter the 1st no:";
+    cin>>n;
+    int r;
+    cout<<"enter the 2nd no:";
+    cin>>r;
+    int ans = fact(n)/(fact(r)*fact(n-r));
+    cout<<ans<<endl;
+    return 0;
+
+}
