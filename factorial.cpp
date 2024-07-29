9e9b550b20e5fddb7ac28a0de5dda60bb45c0f4b
roronoazoro01234 <godzopgamer@gmail.com>
2024-07-29 19:56:49 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+    int fact(int n){
+    int factorial=1;
+    for(int i=2;i<=n;i++){
+        factorial=factorial*i;
+    }
+    return factorial;
+    }
+    
+    int main(){
+       
+       int n;
+       cout<<"enter the no:";
+       cin>>n;
+       int ans=fact(n);
+       cout<<ans<<endl;
+       return 0;
+}
