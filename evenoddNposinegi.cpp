2d98483a7ff432abcd959f26ae58f48d383777e5
roronoazoro01234 <godzopgamer@gmail.com>
2024-08-08 21:00:41 +0530

    Add files via upload

---
+++ evenoddNposinegi.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int a[5]={-2,3,-6,7,-8};
+    
+    for(int i=0;i<5;i++){
+        cout<<a[i];
+        if(a[i]>0){
+            cout<<" is positive no."<<endl;
+             }
+         else if(a[i]%2==0){
+            cout<<" is even no."<<endl;
+            }
+             if(a[i]<0){
+                cout<<" is negative no."<<endl;
+            }
+            else if (a[i]%2==1){
+                cout<<" is odd no."<<endl;
+            }
+            
+        }
+            return 0;
+}
