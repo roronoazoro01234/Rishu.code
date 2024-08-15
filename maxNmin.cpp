c418bb86666bf6a6826516e9e6d71f0724553b06
Rishabh Khanka <godzopgamer@gmail.com>
2024-08-15 20:07:28 +0530

    Add files via upload

---
+++ maxNmin.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  int a[n];
+  for(int i=0;i<n;i++){
+      cin>>a[i];
+  }
+  
+  int maxNo=INT_MIN;
+  int minNo=INT_MAX;
+  
+  for(int i=0;i<n;i++){
+     maxNo=max(maxNo,a[i]);
+     minNo=min(minNo,a[i]);
+  }
+  cout<<maxNo<<" is max no."<<endl<<minNo<<" is min no.";
+ 
+  return 0;
+}
