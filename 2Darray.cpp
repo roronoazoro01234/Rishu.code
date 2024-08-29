de10e47e56f94bfeeec541ce452781766a4533d2
Rishabh Khanka <godzopgamer@gmail.com>
2024-08-29 20:37:08 +0530

    Add files via upload

---
+++ 2Darray.cpp
@@ -0,0 +1,26 @@
+﻿// 2D ARRAY //
+
+#include <iostream>
+using namespace std;
+int main() {
+   int n,m;
+   cout<<"enter the no: ";
+   cin>>n;
+   cout<<"enter the no: ";
+   cin>>m;
+   int arr[n][m];
+   for(int i=0;i<n;i++){
+       for(int j=0;j<m;j++){
+       cin>>arr[i][j];
+           
+       }       
+   }
+   for(int i=0;i<n;i++){
+       for(int j=0;j<m;j++){
+       cout<<arr[i][j]<<" ";
+           
+       } cout<<endl;       
+   }
+
+    return 0;
+}
