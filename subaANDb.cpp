126e64983f8550149ca42dbfff0d197caa7d81f7
Rishabh Khanka <godzopgamer@gmail.com>
2024-08-31 20:43:11 +0530

    Add files via upload

---
+++ subaANDb.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+using namespace std;
+
+int add(int num1, int num2){
+    int sum=num1-num2;
+    return sum;
+}
+int main(){
+     int a;
+     cout<<"enter the 1st no:";
+     cin>>a;
+     int b;
+     cout<<"enter the 2nd no:";
+     cin>>b;
+    cout<<add(a,b);
+    return 0;
+}
