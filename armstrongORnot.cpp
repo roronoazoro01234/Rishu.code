984c06869ea6cced89a4f8127f57896da4d9575f
Rishabh Khanka <godzopgamer@gmail.com>
2024-09-01 19:58:41 +0530

    Add files via upload

---
+++ armstrongORnot.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int sum = 0;
+    int originaln = n;
+    while (n > 0)
+    {
+        int m = n % 10;
+        sum = sum + m * m * m;
+        n = n / 10;
+    }
+    if (sum == originaln)
+    {
+        cout << "Armstrong number" << endl;
+    }
+    else
+    {
+        cout << "Not a Armstrong number" << endl;
+    }
+    return 0;
+}
