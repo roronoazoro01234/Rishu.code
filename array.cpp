4620f716c465aa99dfb1967dab70d212c872b60d
Rishabh Khanka <godzopgamer@gmail.com>
2024-09-02 20:16:23 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+using namespace std;
+
+int main(){
+    int a[5]={10,20,30,40,50};
+     
+      cout<<a[0]<<endl;
+       cout<<a[1]<<endl;
+        cout<<a[2]<<endl;
+         cout<<a[3]<<endl;
+          cout<<a[4]<<endl;
+}
